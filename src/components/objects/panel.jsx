d9a234853f6f262d7a56f0f1b232cfa667155841
ckwan-hub <ckwandrans@gmail.com>
2021-01-26 20:26:24 -0500

    main layout

--- src/components/objects/panel.jsx
+++ src/components/objects/panel.jsx
@@ -1,15 +1,18 @@
 /* @jsx m */
-import m from 'mithril'
+import m from 'mithril';
 
 // "Sidebar" style panel for housing buttons and other components
 
 export const Panel = () => {
-
-    return {
-        view: (vnode) => {
-            return (
-                <div></div>
-            )
-        }
-    }
-}
+  return {
+    view: (vnode) => {
+      return (
+        <div class={vnode.attrs.parentClass} id={vnode.attrs.parentID}>
+          <div class={vnode.attrs.class} id={vnode.attrs.id}>
+            {vnode.attrs.content}
+          </div>
+        </div>
+      );
+    },
+  };
+};
--- src/resources/main.jsx
+++ src/resources/main.jsx
@@ -1,21 +1,46 @@
 /* @jsx m */
-import m from 'mithril'
-import { Test } from '../components/test'
+import m from 'mithril';
+import { Test } from '../components/test';
+import { Screen } from '../components/objects/screen';
+import { Panel } from '../components/objects/panel';
 
 export const Main = () => {
+  let testString = 'Hello World!';
+  let screenInfo = 'TEST';
+  let panel1Content = 'PANEL1TEST';
+  let panel2Content = 'PANEL2TEST';
 
-    let testString = "Hello World!"
-
-    return {
-
-        view: (vnode) => {
-
-            return (
-                <div>
-                    Template for Mithril!
-                    {m(Test, { info: testString })}
-                </div>
-            )
-        }
-    }
-}
+  return {
+    view: (vnode) => {
+      return (
+        <div>
+          Template for Mithril!
+          {m(Test, { info: testString })}
+          {m(Screen, {
+            class: 'screen-parent',
+            id: 'screen-parent',
+            contentClass: 'screen-content',
+            contentID: 'screen-content',
+            content: screenInfo,
+          })}
+          <div class="panel-container">
+            {m(Panel, {
+              class: 'panel1-parent',
+              id: 'panel1-parent',
+              parentClass: 'panel1-content',
+              parentID: 'panel1-content',
+              content: panel1Content,
+            })}
+            {m(Panel, {
+              class: 'panel2-parent',
+              id: 'panel2-parent',
+              parentClass: 'panel2-content',
+              parentID: 'panel2-content',
+              content: panel2Content,
+            })}
+          </div>
+        </div>
+      );
+    },
+  };
+};
